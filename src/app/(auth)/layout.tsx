f129a657fbcb2c4f9c1e57878175dd0a646fe233
Harshal Shinde <shindeharshal009@gmail.com>
2024-06-01 01:08:27 +0530

    suspense added for useSearchParam() error

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full min-h-screen flex justify-center items-center">
-      {children}
-    </div>
+    <Suspense>
+      <div className="w-full min-h-screen flex justify-center items-center">
+        {children}
+      </div>
+    </Suspense>
   );
 }
